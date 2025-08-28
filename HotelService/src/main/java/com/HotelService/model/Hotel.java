30af92167eba3d661f8f8fd044a302f7f06c2c32
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-08-28 15:33:55 +0530

    added the hotel entity in model

---
+++ HotelService/src/main/java/com/HotelService/model/Hotel.java
@@ -0,0 +1,66 @@
+package com.HotelService.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="micro-hotel")
+public class Hotel {
+    @Id
+    private String hotelId;
+    private String name;
+    private String location;
+    private String about;
+
+
+    //constructor
+
+
+    public Hotel() {
+    }
+
+    public Hotel(String hotelId, String name, String location, String about) {
+        this.hotelId = hotelId;
+        this.name = name;
+        this.location = location;
+        this.about = about;
+    }
+
+
+
+    //getter setter
+
+    public String getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(String hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+}
