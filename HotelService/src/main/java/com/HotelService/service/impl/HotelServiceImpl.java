c7c199de2bfd432a4fbeb0d6631739aaf89a6bb5
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-08-28 15:51:13 +0530

    implemented the methods

--- HotelService/src/main/java/com/HotelService/service/impl/HotelServiceImpl.java
+++ HotelService/src/main/java/com/HotelService/service/impl/HotelServiceImpl.java
@@ -1,2 +1,31 @@
-package com.HotelService.service.impl;public class HotelServiceImpl {
+package com.HotelService.service.impl;
+
+import com.HotelService.model.Hotel;
+import com.HotelService.repository.HotelRepo;
+import com.HotelService.service.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HotelServiceImpl implements HotelService {
+
+    @Autowired
+    private HotelRepo hotelRepo;
+
+    @Override
+    public Hotel create(Hotel hotel) {
+        return hotelRepo.save(hotel);
+    }
+
+    @Override
+    public List<Hotel> getAll() {
+        return hotelRepo.findAll();
+    }
+
+    @Override
+    public Hotel getById(String hotelId) {
+        return hotelRepo.findById(hotelId).orElseThrow(()->new ResourceNotFound("hotel with given id not found:"+hotelId));
+    }
 }
