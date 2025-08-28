0cfb4498c8dd4d9f903bd577443621b801ddd323
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-08-28 16:07:18 +0530

    added the random unique id for hotel

--- HotelService/src/main/java/com/HotelService/service/impl/HotelServiceImpl.java
+++ HotelService/src/main/java/com/HotelService/service/impl/HotelServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class HotelServiceImpl implements HotelService {
@@ -17,6 +18,9 @@ public class HotelServiceImpl implements HotelService {
 
     @Override
     public Hotel create(Hotel hotel) {
+        //random unique id
+        String randomHotelId=UUID.randomUUID().toString();
+        hotel.setHotelId(randomHotelId);
         return hotelRepo.save(hotel);
     }
 
