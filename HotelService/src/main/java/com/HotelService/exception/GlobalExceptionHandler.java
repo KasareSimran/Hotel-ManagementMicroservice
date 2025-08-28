e97dcdb968df3bb60c67ee5922c51fa5d20f5ec9
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-08-28 16:16:22 +0530

    added the global exception handler

--- HotelService/src/main/java/com/HotelService/exception/GlobalExceptionHandler.java
+++ HotelService/src/main/java/com/HotelService/exception/GlobalExceptionHandler.java
@@ -1,2 +1,25 @@
-package com.HotelService.exception;public class GlobalExceptionHandler {
+package com.HotelService.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFound.class)
+    public ResponseEntity<Map<String,Object>> notFoundHandler(ResourceNotFound ex){
+        Map map=new HashMap<>();
+        map.put("message",ex.getMessage());
+        map.put("status",false);
+        map.put("status", HttpStatus.NOT_FOUND);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map);
+
+
+    }
 }
