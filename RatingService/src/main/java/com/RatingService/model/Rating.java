0111db55a9344fd390a8f88f5171fcd363661d8f
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-08-29 13:59:05 +0530

    updated

---
+++ RatingService/src/main/java/com/RatingService/model/Rating.java
@@ -0,0 +1,2 @@
+package com.RatingService.model;public class Rating {
+}
--- UserService/src/main/java/com/UserService/model/Rating.java
+++ UserService/src/main/java/com/UserService/model/Rating.java
@@ -3,10 +3,9 @@ package com.UserService.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-@Entity
+
 public class Rating {
 
-    @Id
     private String ratingId;
     private String userId;
     private String hotelId;
