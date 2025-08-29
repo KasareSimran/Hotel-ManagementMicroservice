50bd43e6647e41c4adf7c57e52a6308f8d09e70b
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-08-29 14:03:22 +0530

    created the enitiy for rating

--- RatingService/src/main/java/com/RatingService/model/Rating.java
+++ RatingService/src/main/java/com/RatingService/model/Rating.java
@@ -1,2 +1,72 @@
-package com.RatingService.model;public class Rating {
+package com.RatingService.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="micro-rating")
+public class Rating {
+
+    @Id
+    private String ratingId;
+    private String userId;
+    private String hotelId;
+    private String rating;
+    private String feedback;
+
+    //constructor
+    public Rating() {
+    }
+
+    public Rating(String ratingId, String userId, String hotelId, String rating, String feedback) {
+        this.ratingId = ratingId;
+        this.userId = userId;
+        this.hotelId = hotelId;
+        this.rating = rating;
+        this.feedback = feedback;
+    }
+
+    //getter setter
+
+
+    public String getRatingId() {
+        return ratingId;
+    }
+
+    public void setRatingId(String ratingId) {
+        this.ratingId = ratingId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(String hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
 }
